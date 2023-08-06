6c29966f4d53c64583d7d1b4d0f37d31cc30539f
fatimasouliman <fffaat.sss@gmail.com>
2023-08-06 07:55:13 +0300

    first commit

---
+++ src/data/user.js
@@ -0,0 +1,59 @@
+const INFO = {
+	main: {
+		title: "Reactfolio by truethari",
+		name: "Tharindu N.",
+		email: "fffaat.sss@gmail.com",
+		logo: "../logo.jpg",
+	},
+
+	socials: {
+		twitter: "https://twitter.com/",
+		github: "https://github.com/fatimasouliman",
+		linkedin: "https://www.linkedin.com/in/fatima-souliman/",
+		instagram: "https://instagram.com/",
+		stackoverflow: "https://stackoverflow.com/",
+		facebook: "https://facebook.com/",
+	},
+
+	homepage: {
+		title: "Front-end developer",
+		description:
+			"Interested in smallest details , so of course I have the ability to pay attention to details. I made my way as a front-end developer about a year and a half ago. I started with self-learning until I got trained at focal X Agency.It's great and perfect when I can solve a complex problem that has been holding up my project for hours.I can work on my own, plus I'm good at communicating and working as a team.",
+	},
+
+	about: {
+		title: "I’m Fatima. I live in Syria, where I design the future.",
+		description:
+			"I've worked on a wide variety of projects over the past 10 months, and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
+	},
+
+	articles: {
+		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+	},
+
+	projects: [
+		{
+			title: "Project 1",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "react.png",
+			linkText: "View Project",
+			link: "https://fatimasouliman.github.io/fatima-souliman-task1/",
+		},
+
+		{
+			title: "Project 2",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "bootstrap.png",
+			linkText: "View Project",
+			link: "https://fatimasouliman.github.io/Cake-bakery/",
+		},
+
+	
+	],
+};
+
+export default INFO;
